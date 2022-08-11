fe0453f29de3974ba9501738ab7855aa6552c1cf
Andrew S <andrewsales2004@gmail.com>
2022-08-11 06:23:56 -0500

    first question now pops up when start quiz button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,11 @@ var StartQuizbtn = document.querySelector('.Start-btn').addEventListener('click'
 
     }, 1000);
 
-    var introTitle = document.querySelector("#questions:first-child")
+});
+
+var StartQuiz = document.getElementById('Start-btn').addEventListener('click', function(){
+
+    var introTitle = document.querySelector("#questions:first-child");
 
     var queTitle = document.createElement('h1');
 
@@ -33,15 +37,41 @@ var StartQuizbtn = document.querySelector('.Start-btn').addEventListener('click'
 
     introTitle.parentNode.replaceChild(queTitle, introTitle);
 
-    queTitle.className = "Quiz-info-box Question-title"
+    queTitle.className = "Quiz-info-box Question-title";
+
+    var answer1El = document.createElement("button");
+
+    answer1El.innerHTML ='1st answer choice';
+
+    queTitle.appendChild(answer1El);
+
+    answer1El.className = "answers one wrong"
+
+    var answer2El = document.createElement("button")
+
+    answer2El.innerHTML ='2nd answer choice';
+
+    queTitle.appendChild(answer2El);
+
+    answer2El.className = "answers two wrong"
+
+    var answer3El = document.createElement("button")
+
+    answer3El.innerHTML ='3rd answer choice';
+
+    queTitle.appendChild(answer3El);
+
+    answer3El.className = "answers three wrong"
+
+    var answer4El = document.createElement("button")
+
+    answer4El.innerHTML ='4th answer choice';
+
+    queTitle.appendChild(answer4El);
+
+    answer4El.className = "answers four wrong"
+
 });
 
 
 
-
-
-
-    var answer1EL = document.createElement("button")
-    var answer2EL = document.createElement("button")
-    var answer3EL = document.createElement("button")
-    var answer4EL = document.createElement("button")
